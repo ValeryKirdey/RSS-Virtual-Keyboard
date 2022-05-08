49601e831cc5056969b4c1560a470aff7e76d68d
ValeryKirdey <v.a.l.e.r.a95@mail.ru>
2022-05-08 23:21:04 +0300

    feat: add ipad img to project

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
+// const image = document.createElement('img')
+// image.src = "../img/IPad_mini_3_mockup.png"
+// document.querySelector("body").appendChild(image)
+
+// const img = new Image();
+// img.src = '../img/IPad_mini_3_mockup.png'
+// pc.appendChild(img);
+
+// var pc = document.getElementById("pic_cntr");
+// pc.innerHTML = "<img src='../img/IPad_mini_3_mockup.png'>";
+
+// window.onload = function() {
+//         var pc = document.getElementById("pic_cntr");
+//         pc.innerHTML = "<img src='../img/IPad.png'>";
+//     }
 console.log("start task: 01.05.2022")
